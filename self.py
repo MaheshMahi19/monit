1aab51ccfa819b483229e7e6aa7e7c0caf916488
Mahesh19 <30853110+MaheshMahi19@users.noreply.github.com>
2020-02-21 16:56:11 +0530

    Update self.py

--- self.py
+++ self.py
@@ -13,7 +13,7 @@ async def on_message(message):
     #cl = [545791218743902220] #channels ID's #general
     #cs = client.get_channel(560423218184192021) #channel id to send messages for to be copied by bot #test
 
-    #md = {"680321947166572554":"https://discordapp.com/api/webhooks/680357110269476869/P2UuUtbRa6ufx2m8q8Lg48Arx5aCVqSVlDmS26jY08tgCJnkLlqewyTm21QHpJE5oLFw","680321976539283566":"https://discordapp.com/api/webhooks/680357508992598026/2tfriUvjG5juqm-yL9bZeJ3y5WdPHejdCyGS5FiscIuEbBelc38Cy78k9jMxeSH0iRkc"}
+    md = {"680321947166572554":"https://discordapp.com/api/webhooks/680357110269476869/P2UuUtbRa6ufx2m8q8Lg48Arx5aCVqSVlDmS26jY08tgCJnkLlqewyTm21QHpJE5oLFw","680321976539283566":"https://discordapp.com/api/webhooks/680357508992598026/2tfriUvjG5juqm-yL9bZeJ3y5WdPHejdCyGS5FiscIuEbBelc38Cy78k9jMxeSH0iRkc"}
     if str(message.channel.id) in md:
         wbId,wbTok = md[str(message.channel.id)].split("/")[-2],md[str(message.channel.id)].split("/")[-1]
         wb = Webhook.partial(int(wbId),wbTok,adapter=RequestsWebhookAdapter())
